8048ed014d7278a629e074ba074d37711ddc6342
pehilbert <148654775+pehilbert@users.noreply.github.com>
2025-09-09 15:00:21 -0700

    Add field study opt in option, make chat logging true by default

--- src/personalization.ts
+++ src/personalization.ts
@@ -1,15 +1,15 @@
 import getApiUrl from "./apiConfig";
 import { authenticateWithGitHub } from "./auth";
-import {AxiosError, get, put} from "axios";
+import {AxiosError, get, post, put} from "axios";
 import * as vscode from 'vscode';
 import { Axios } from "axios";
 
-export async function syncPersonalization(context: vscode.ExtensionContext) {
+export async function syncSigilSettings(context: vscode.ExtensionContext) {
     try {
         const githubUser = await authenticateWithGitHub(context);
 
         if (!githubUser) {
-            vscode.window.showErrorMessage("Sigil: Authentication required to sync personalization settings");
+            vscode.window.showErrorMessage("Sigil: Authentication required to sync settings");
             return;
         }
 
@@ -18,12 +18,17 @@ export async function syncPersonalization(context: vscode.ExtensionContext) {
 
         const config = vscode.workspace.getConfiguration();
         await config.update('sigil.personalizedPrompt', personalization.personalizedPrompt, vscode.ConfigurationTarget.Global);
-        vscode.window.showInformationMessage("Sigil: Personalization settings synced successfully");
+
+        const optInResult = await get(`${getApiUrl()}/api/users/getFieldStudyOptInStatus/${githubUser.id}`);
+        const optIn = result.data.fieldStudyOptIn || false;
+        await config.update('sigil.fieldStudyOptIn', optIn, vscode.ConfigurationTarget.Global);
+
+        vscode.window.showInformationMessage("Sigil: User settings synced successfully");
     } catch (error: AxiosError | any) {
         // No need to show a message if they simply don't have any personalization settings (404)
         // but show an error for other issues (e.g. network errors, server errors, etc.)
         if (error?.response && error?.response?.status !== 404) {
-            vscode.window.showErrorMessage("Sigil: Error syncing personalization settings");
+            vscode.window.showErrorMessage("Sigil: Error syncing user settings");
         }
     }
 }
@@ -33,7 +38,7 @@ export async function updatePersonalization(context: vscode.ExtensionContext, ne
         const githubUser = await authenticateWithGitHub(context);
 
         if (!githubUser) {
-            vscode.window.showErrorMessage("Sigil: Authentication required to update personalization settings");
+            vscode.window.showErrorMessage("Sigil: Authentication required to update settings");
             return;
         }
 
@@ -41,5 +46,21 @@ export async function updatePersonalization(context: vscode.ExtensionContext, ne
         vscode.window.showInformationMessage("Sigil: Personalization settings updated successfully");
     } catch (error) {
         vscode.window.showErrorMessage("Sigil: Error updating personalization settings");
+    }
+}
+
+export async function updateOptIn(context: vscode.ExtensionContext, newSetting: boolean) {
+    try {
+        const githubUser = await authenticateWithGitHub(context);
+
+        if (!githubUser) {
+            vscode.window.showErrorMessage("Sigil: Authentication required to update settings");
+            return;
+        }
+
+        await post(`${getApiUrl()}/api/users/changeFieldStudyOptInStatus`, { userID: githubUser.id, fieldStudyOptIn: newSetting });
+        vscode.window.showInformationMessage("Sigil: Opt in status updated successfully");
+    } catch (error) {
+        vscode.window.showErrorMessage("Sigil: Error updating opt in status");
     }
 }
